a47c5e2af625633fcf31549beee4c1ce4551fd6b
laozefeng <laozf@77ircloud.com>
2021-03-30 09:58:30 +0800

    feat: 第一题的三种解题方式

--- code/1.两数之和.ts
+++ code/1.两数之和.ts
@@ -6,10 +6,35 @@
 
 // @lc code=start
 function twoSum(nums: number[], target: number) {
-  let j: number = 0;
+  let j: number = 0
   for (let i = 0; i < nums.length; i++) {
-    j = nums.slice(i + 1).findIndex((el) => el === target - nums[i]) + i + 1;
-    if (i !== j) return [i, j];
+    j = nums.slice(i + 1).findIndex((el) => el === target - nums[i]) + i + 1
+    if (i !== j) return [i, j]
+  }
+}
+// @lc code=end
+
+// @lc code=start
+function twoSum_(nums: number[], target: number) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j]
+      }
+    }
+  }
+}
+// @lc code=end
+
+// @lc code=start
+function twoSum__(nums: number[], target: number) {
+  let result = new Map()
+  for (let i = 0; i < nums.length; i++) {
+    let current = target - nums[i]
+    if (result.has(current)) {
+      return [result.get(current), i]
+    }
+    result.set(nums[i], i)
   }
 }
 // @lc code=end
