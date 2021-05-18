cec2380bc2f8f667cb431918631b506457aef2fe
laozefeng <laozf@77ircloud.com>
2021-05-18 09:13:55 +0800

    172.阶乘后的零

---
+++ test/172.阶乘后的零.spec.ts
@@ -0,0 +1,17 @@
+import trailingZeroes from '../code/172.阶乘后的零'
+
+test('172.阶乘后的零', () => {
+  expect(trailingZeroes(3)).toBe(0)
+})
+
+test('172.阶乘后的零', () => {
+  expect(trailingZeroes(5)).toBe(1)
+})
+
+test('172.阶乘后的零', () => {
+  expect(trailingZeroes(11)).toBe(2)
+})
+
+test('172.阶乘后的零', () => {
+  expect(trailingZeroes(16)).toBe(3)
+})
